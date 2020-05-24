2b70014d0976eaeeb77482baf763792fbeb55d35
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 19:34:18 -0400

    Complete 3 checks

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,13 @@
 function titleCaseEdit(title) {
   // Insert code here;
+
+  var capitalized = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+  var words = title.split(' ').map(capitalized);
+
+  capitalized = words.join(' ')
+
+  return capitalized;
 }
 
 // Do not edit this line;
