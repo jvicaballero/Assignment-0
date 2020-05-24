cc8f14e5a777d90672ec3c5f22777f57a66f9268
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 19:50:37 -0400

    Completed 3 checks

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,18 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var numBools = 0;
+  var checkTrue = true;
+  var checkFalse = false;
+  for (var i = 0 ; i < arr.length; i++)
+  {
+    if (arr[i] === checkFalse)
+        numBools++;
+
+      else if(arr[i] === checkTrue)
+      numBools++;
+  }
+
+  return numBools;
 }
 
 // Do not edit this line;
