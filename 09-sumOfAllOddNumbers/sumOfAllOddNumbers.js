c486350404e7b015412f6a5a319bbdeeff431e65
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 20:14:37 -0400

    Completed 4 checks

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  // Insert code here;  
+  var numOdds = 0;
+  for(var i = 0; i < nums.length; i++)
+  {
+    if (nums[i] % 2 != 0)
+      numOdds++;
+  }
+
+  return numOdds;
 }
 
 // Do not edit this line;
