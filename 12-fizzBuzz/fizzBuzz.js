108ea9b4788ec9bccc21ba3bca06a917da62f523
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 21:24:37 -0400

    Complete 4 checks

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,24 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  let myArray = [];
+
+  for(var i = start; i <= end; i++)
+  {
+    if (i % 5 === 0 && i % 3 === 0)
+      myArray.push("FizzBuzz");
+    
+      else if(i % 3 == 0)
+        myArray.push("Fizz");
+      
+      else if(i % 5 == 0)
+        myArray.push("Buzz");
+
+        else myArray.push(i);
+      
+        
+  }
+
+  return myArray;
 }
 
 // Do not edit this line;
