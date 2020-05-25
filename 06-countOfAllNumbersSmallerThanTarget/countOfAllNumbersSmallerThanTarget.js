c5c41565e8da724b7a270ee9430fae53e43c6ef9
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 20:05:08 -0400

    Complete 4 checks

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,13 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  var belowTarget = 0;
+  for(var i = 0 ; i < nums.length; i++)
+{
+  if(nums[i] < target)
+    belowTarget++;
+}
+
+return belowTarget;
 }
 
 // Do not edit this line;
