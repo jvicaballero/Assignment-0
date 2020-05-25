265722c8f9cffbb8f0b8d9a060f061e7c0eadedd
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 22:22:02 -0400

    Complete 4 checks

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,17 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+
+  var mySet = new Set();
+
+  for(var set of args)
+  {
+    for (var num of set)
+    {
+      mySet.add(num);
+    }
+  }
+
+  return mySet;
 }
 
 // Do not edit this line;
