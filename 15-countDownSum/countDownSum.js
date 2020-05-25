b6af85160e7bbf4d068e09fce3c0a59795b7f11f
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 22:08:08 -0400

    Complete 4 checks

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,12 @@
 class MySolution {
-  countDownSum(num) {
+  countDownSum(num , currentSum = 0) {
     // Insert code here;
+    
+    if (num == 1)
+      return currentSum += 1;
+
+    else
+       return this.countDownSum(num - 1, currentSum += num);
   }
 }
 
