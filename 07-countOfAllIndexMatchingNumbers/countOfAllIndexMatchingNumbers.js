6848c4242a5abda3b4e1fd60ffd6f9f7ea39cfa3
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 20:08:42 -0400

    Complete 4 checks

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,13 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  var matches = 0;
+  for (var i = 0; i < nums.length; i++)
+  {
+    if(nums[i] == i)
+    matches++;
+  }
+
+  return matches;
 }
 
 // Do not edit this line;
