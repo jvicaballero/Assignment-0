feb56ae0a88cd886b5d0df942fca12e8458959a9
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 22:29:51 -0400

    Complete 7 checks

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,22 @@
 function pairSum(nums, target) {
   // Insert code here;
+
+  if(nums.length <= 1)
+    throw "Array is empty or only has 1 element";
+
+    var val = 0;
+  for (var i = 0; i < nums.length; i++)
+  {
+    val = nums[i];
+
+    for(var j = 0; j <nums.length; j++)
+    {
+      if (nums[i] + nums[j] == target)
+        return true;
+    }
+  }
+
+  return false;
 }
 
 // Do not edit this line;
