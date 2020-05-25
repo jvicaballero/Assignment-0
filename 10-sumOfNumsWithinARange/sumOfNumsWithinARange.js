cfeefad9486db169ec3798b200b402a41dd889cd
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 20:23:28 -0400

    Complete with 4 checks

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,16 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+
+  var inRange = 0;
+
+  for(var i =0; i < nums.length; i++)
+  {
+    if(nums[i] >= start && nums[i] <= end)
+      inRange++;
+
+  }
+
+  return inRange;
 }
 
 // Do not edit this line;
