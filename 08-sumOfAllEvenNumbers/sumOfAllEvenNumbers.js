6bc571e4716b13c1d1ba0630031c05e53b36d032
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 20:11:51 -0400

    Complete 4 checks

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  var numEvens = 0;
+  for(var i = 0; i < nums.length; i++)
+  {
+    if (nums[i] % 2 == 0)
+      numEvens++;
+  }
+
+  return numEvens;
 }
 
 // Do not edit this line;
