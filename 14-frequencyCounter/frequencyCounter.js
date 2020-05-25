945bfde0eed844ca87ebadd0cc96cf7afc35d4d9
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 21:55:11 -0400

    Complete 4 checks

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,19 @@
 function frequencyCounter(word) {
   // Insert code here;
+
+  let myArray = {};
+
+  for(var i = 0; i < word.length; i++)
+  {
+   
+    if(myArray[word[i]])
+      myArray[word[i]] += 1;
+
+    else 
+      myArray[word[i]] = 1;
+  }
+
+  return myArray;
 }
 
 // Do not edit this line;
