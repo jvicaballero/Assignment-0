52cab20a58fc15a8454ed2afaf61169781c8a8cf
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 20:00:51 -0400

    Completed 4 checks

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,22 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var checkTrue = true;
+  var checkFalse = false;
+  var numBoolNString = 0;
+
+  for (var i = 0; i <arr.length; i++)
+  {
+    if(arr[i] === checkFalse)
+      numBoolNString++;
+
+    else if (arr[i] === checkTrue)
+      numBoolNString++;
+
+    else if(typeof(arr[i]) == "string")
+      numBoolNString++;
+  }
+
+  return numBoolNString;
 }
 
 // Do not edit this line;
