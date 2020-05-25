c626b317a49466d751849bd154aab4eb88eae364
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 22:14:10 -0400

    Complete 4 checks

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,14 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+
+  var product = 1;
+
+  for (var i = 0; i < args.length; i++)
+  {
+    product *= args[i];
+  }
+
+  return product;
 }
 
 // Do not edit this line;
