ac8c3e6bfc963beee0c2d28cc702f19854328783
Jasper Caballero <jvicaballero@gmail.com>
2020-05-24 21:37:35 -0400

    Complete 4 checks

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,20 @@
 function isPalindrome(word) {
   // Insert code here;
+
+  var start = 0;
+  var end = word.length - 1;
+
+  for(var i = 0 ; i < word.length; i++)
+  {
+    if(word[i] != word[end])
+      return false;
+
+    end--;
+      
+      
+  }
+
+  return true;
 }
 
 // Do not edit this line;
