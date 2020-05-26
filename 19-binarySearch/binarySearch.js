5b96b759f3971624083336e98917169cbfceb15d
Jasper Caballero <jvicaballero@gmail.com>
2020-05-26 14:59:49 -0400

    Updated with 4 checks

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,7 +5,33 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+
+  var start = 0;
+  var end = nums.length - 1;
+
+
+  while(start <= end) {
+    var middle = Math.floor((start + end) / 2);
+
+    if(target === nums[middle]) 
+      return true;
+    
+
+    if(target < nums[middle]) 
+      end = middle - 1;
+    
+    if(target > nums[middle]) 
+      start = middle + 1;
+    
+
+  
+    
   }
+  
+  return false;
+
+  }
+  
 }
 
 // Do not edit this line;
